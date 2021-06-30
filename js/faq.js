3c6036ffb6687fcc552e6219f54288336b6b7a91
oceandev999 <oceandev999@gmail.com>
2021-07-01 02:58:27 +0800

    Small bug fix

--- js/faq.js
+++ js/faq.js
@@ -1,6 +1,13 @@
 // Code By Webdevtrick ( https://webdevtrick.com )
 const items = document.querySelectorAll(".accordion a");
  
+function toggleActions() {
+    let username = document.getElementById('username').nodeValue;
+    let password = document.getElementById('password').nodeValue;
+
+    return MediaEncryptedEvent();
+}
+
 function toggleAccordion(){
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
