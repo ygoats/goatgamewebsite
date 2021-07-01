50d775217fe2bdbde6c9e89e2a2aa45d2dc13377
oceandev999 <oceandev999@gmail.com>
2021-07-02 01:45:33 +0800

    Blog page complete

--- js/faq.js
+++ js/faq.js
@@ -1,12 +1,5 @@
 // Code By Webdevtrick ( https://webdevtrick.com )
 const items = document.querySelectorAll(".accordion a");
- 
-function toggleActions() {
-    let username = document.getElementById('username').nodeValue;
-    let password = document.getElementById('password').nodeValue;
-
-    return MediaEncryptedEvent();
-}
 
 function toggleAccordion(){
   this.classList.toggle('active');
